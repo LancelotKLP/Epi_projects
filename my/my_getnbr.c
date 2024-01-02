093409c53ff7dca18dea221d802ed136e25490e9
Maël KEMPF-LE PAPE <mael.kempf-le-pape@epitech.eu>
2024-01-02 16:08:13 +0100

    cs lib

--- my/my_getnbr.c
+++ my/my_getnbr.c
@@ -20,11 +20,11 @@ int main(void)
     int nb = 0;
     int neg = 1;
 
-    for (;str[i] == '-' || str[i] == '+'; i++) {
+    for (; str[i] == '-' || str[i] == '+'; i++) {
         if (str[i] == '-')
             neg = neg * -1;
     }
-    for (;str[i] >= '0' && str[i] <= '9'; i++) 
+    for (; str[i] >= '0' && str[i] <= '9'; i++)
         nb = nb * 10 + (str[i] -48);
     printf("%d\n", nb * neg);
     return (nb * neg);
